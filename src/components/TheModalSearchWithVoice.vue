4df50ca51e08d569985e2aa20c1a9c3550aa57e8
Ibrakhimzhanov <unkiborg@gmail.com>
2022-10-11 17:33:56 +0500

    перезаписал кнопку в новый компонент

--- src/components/TheModalSearchWithVoice.vue
+++ src/components/TheModalSearchWithVoice.vue
@@ -1,113 +1,23 @@
 <template>
   <BaseModal>
     <p class="text-2xl mb-52">{{ text }}</p>
-    <div class="flex justify-center items-center">
-      <span v-show="isListening" :class="buttonAnimationClasses" />
-      <button :class="buttonClassses" @click="toggleRecording">
-        <BaseIcon name="microphone" />
-      </button>
-    </div>
-    <div :class="buttonHindClassses">Tap the microphone to try again</div>
+    <TheButtonSearchWithVoice @change-text="text = $event" />
   </BaseModal>
 </template>
+
 <script>
-import BaseIcon from "./BaseIcon.vue";
 import BaseModal from "./BaseModal.vue";
+import TheButtonSearchWithVoice from "./TheButtonSearchWithVoice.vue";
 
 export default {
   components: {
     BaseModal,
-    BaseIcon,
+    TheButtonSearchWithVoice,
   },
   data() {
     return {
-      isQuiet: false,
-      isListening: true,
-      isRecording: false,
-      recordingTimeout: null,
+      text: null,
     };
-  },
-  computed: {
-    buttonClassses() {
-      return [
-        this.isListening ? "bg-red-600" : "bg-gray-300",
-        this.isListening ? "text-white" : "text-black",
-        "w-16",
-        "h-16",
-        "mx-auto",
-        "rounded-full",
-        "flex",
-        "justify-center",
-        "items-center",
-        "relative",
-        "focus:outline-none",
-      ];
-    },
-
-    buttonHindClassses() {
-      return [
-        this.isListening ? "invisible" : "visible",
-        "text-center",
-        "text-sm",
-        "text-gray-500",
-        "mt-4",
-      ];
-    },
-
-    text() {
-      if (this.isQuiet) {
-        return "Didn't hear that. Try again.";
-      }
-
-      if (this.isListening || this.isRecording) {
-        return "Listening...";
-      }
-
-      return "Microphone off. Try again.";
-    },
-
-    buttonAnimationClasses() {
-      return [
-        this.isRecording ? "bg-gray-300" : "border border-gray-300",
-        "animate-ping",
-        "absolute",
-        "w-14",
-        "h-14",
-        "rounded-full",
-      ];
-    },
-  },
-  mounted() {
-    this.handleRecordingTimeout();
-  },
-  beforeUnmount() {
-    clearTimeout(this.recordingTimeout);
-  },
-  methods: {
-    toggleRecording() {
-      clearTimeout(this.recordingTimeout);
-      this.isQuiet = false;
-
-      if (this.isRecording) {
-        this.isRecording = false;
-        this.isListening = false;
-      } else if (this.isListening) {
-        this.isRecording = true;
-      } else {
-        this.isListening = true;
-      }
-
-      this.handleRecordingTimeout();
-    },
-    handleRecordingTimeout() {
-      if (this.isListening || this.isRecording) {
-        this.recordingTimeout = setTimeout(() => {
-          this.isQuiet = true;
-          this.isListening = false;
-          this.isRecording = false;
-        }, 5000);
-      }
-    },
   },
 };
 </script>
