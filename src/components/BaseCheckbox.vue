b15cc8350ddac6169eb91a468631e99e6bcc405c
Ibrakhimzhanov <unkiborg@gmail.com>
2022-10-12 13:39:53 +0500

    Вынес чекбоксы в базовый компонент

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    class="h-5 w-5 cursor-pointer"
+    type="checkbox"
+    :id="id"
+    v-bind="$attrs"
+    v-model="modelValue"
+  />
+  <label v-if="$slots.default" :for="id" class="pl-4 cursor-pointer flex-grow">
+    <slot />
+  </label>
+</template>
+<script>
+export default {
+  props: {
+    id: Number,
+    modelValue: Array,
+  },
+};
+</script>
--- src/components/TheModalSearchPredictions.vue
+++ src/components/TheModalSearchPredictions.vue
@@ -1,24 +1,10 @@
 <template>
   <BaseModal class="md:w-4/5 max-w-2xl">
     <p class="text-base text-black tracking-wide">Report search predictions</p>
-    <div class="space-y-3 text-black text-sm mt-5">
-      <div
-        class="flex items-center"
-        v-for="(label, index) in searchPredictions"
-        :key="label"
-      >
-        <input
-          class="h-5 w-5 cursor-pointer"
-          type="checkbox"
-          :id="index"
-          :value="label"
-          v-model="selectedSearchPredictions"
-        />
-        <label :for="index" class="pl-4 cursor-pointer flex-grow">{{
-          label
-        }}</label>
-      </div>
-    </div>
+    <TheSearchPredictionsList
+      :search-predictions="searchPredictions"
+      v-model="selectedSearchPredictions"
+    />
     <template #footer="{ close }">
       <button @click="close" :class="classes">Cancel</button>
       <button :class="ReportClasses" disabled>Report</button>
@@ -28,30 +14,19 @@
 
 <script>
 import BaseModal from "./BaseModal.vue";
-
+import TheSearchPredictionsList from "./TheSearchPredictionsList.vue";
 export default {
   components: {
     BaseModal,
+    TheSearchPredictionsList,
   },
+  props: {
+    searchPredictions: Array,
+  },
+
   data() {
     return {
       selectedSearchPredictions: [],
-      searchPredictions: [
-        "new york giants",
-        "new york alicia keys",
-        "new york giants vs washington footbal",
-        "new york",
-        "new york song",
-        "new york new york frank sinatra",
-        "new york jets",
-        "new york city",
-        "new york giants live",
-        "new york state of mind",
-        "new york giants vs washington footbal live",
-        "new york giants injury",
-        "new york giants live stream",
-        "new york accent",
-      ],
       classes: [
         "uppercase",
         "text-sm",
